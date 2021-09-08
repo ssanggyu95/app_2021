e9f0c83fe75f8948a22e6e6aca3c1679cf54ce9c
unknown <thdtkdrb@ajou.ac.kr>
2021-09-08 11:15:44 +0900

    project base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}){
+  
+  return <h1> I like {fav}</h1>;
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      <h1>Hi?</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="삼겹살" />
+      <Food fav="쭈꾸미" />
     </div>
-  );
+    );
 }
 
 export default App;
